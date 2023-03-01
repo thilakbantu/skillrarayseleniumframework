eb436a72f65bdae715e4462afc01077078af4350
preethisrinivasan <preethisrinivasan@Thilak-Bantu>
2023-03-01 13:18:06 +0530

    added commit

--- src/test/java/testScript/ThridTest.java
+++ src/test/java/testScript/ThridTest.java
@@ -8,16 +8,32 @@ import pompages.Homepage;
 public class ThridTest extends baseClass {
 
 	
-	public void thridTest()
+	public void thridTest() throws InterruptedException
 	{
 		SoftAssert soft=new SoftAssert();
-		home.searchFor("core java for selenium");
-		soft.assertEquals(corejava.getPageHeader(), "CORE JAVA FOR SELENIUM");
-		
-		core
-		soft.assertEquals(javavideo.getPageHeader(), "core Java For Selenium Traing");
-		
+		 
+		  home.searchFor("core java for selenium");
+		  soft.assertEquals(corejava.getPageHeader(),"CORE JAVA FOR SELENIUM");
+		  
+		  corejava.clickCoreJavaForSeleniumLink();
+		  soft.assertEquals (javavideo.getPageHeader(),"Core Java For Selenium Training");
+		  
+		  
+		  javavideo.clickCloseCookies();
+		  web.switchToFrame();
+		  
+		  javavideo.clickplayButton();
+		  Thread.sleep(2000);
+		  javavideo.clickpauseButton();
+		  
+		  web.switchBackFromFrame();
+		  
+		  javavideo.clickAddToWishlist();
+		  
+		  soft.assertAll();
+	  }
+	
 		
 		
 	}
-}
+
